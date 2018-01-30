ad494fa2e0b174b0987dc9ca58f21b13e378d237
monicahedda <monica.hedda@vanderbilt.edu>
2018-01-29 21:49:18 -0600

    create files in new directory

--- split_data.py
+++ split_data.py
@@ -2,22 +2,37 @@ import csv
 import os
 
 
-def read_file_by_line(file_name):
+def read_file_by_line(file_name, size, dir_name):
 	in_file = open(file_name, 'r')
         in_reader = csv.reader(in_file) 
+
 	i = 0
+
+	create_directory(dir_name) #create directory for smaller files
+
 	file_name = file_name.replace('.csv', '')
-	out_file_name = file_name + str(i) + '.csv'
+	out_file_name = dir_name + '/' + file_name + str(i) + '.csv'
 
 	is_full = False
 	
 	for row in in_reader:
-	    is_full = check_file_size(out_file_name)
+	    is_full = check_file_size(out_file_name, size)
 	    if not is_full:
 	        is_full = write_line_to_file(row, out_file_name)
 	    else:
 		i = i + 1
-		out_file_name = file_name + str(i) + '.csv'
+		out_file_name = dir_name + '/' + file_name + str(i) + '.csv'
+		write_line_to_file(row, out_file_name)
+
+	print 'Created ', i , ' smaller files.'
+
+
+def create_directory(dir_name):
+	try:
+    	    os.makedirs(dir_name)
+	except OSError:
+    	    pass
+
 
 def write_line_to_file(row, out_file_name):
 	out_file = open(out_file_name, 'a')
@@ -25,12 +40,13 @@ def write_line_to_file(row, out_file_name):
 	n_writer.writerow(row)
 	out_file.close()
 
-def check_file_size(out_file_name):
+
+def check_file_size(out_file_name, size):
 	if os.path.isfile(out_file_name):
         	file_info = os.stat(out_file_name)
 		size_in_MB = convert_bytes(file_info.st_size, 'MB')
 		#print 'size_in_MB=',size_in_MB
-        	if size_in_MB >= 16:
+        	if size_in_MB >= size:
             	    return True
         	else:
             	    return False
@@ -50,7 +66,7 @@ def convert_bytes(num, type):
 	    num = num/1024.0
 
 def main():
-	read_file_by_line('CUSTOMER.csv')
+	read_file_by_line('CUSTOMER.csv', 16, 'CUSTOMER') # enter filename, size in MB here, 
 	#print convert_bytes(10000000, 'MB') 
 
 if __name__ == '__main__':
